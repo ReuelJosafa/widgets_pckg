1cccc8840afa6ace7c24f25529cb7329ec8d29ba
ReuelJosafa <78435939+ReuelJosafa@users.noreply.github.com>
2022-07-26 11:04:07 -0300

    upd: antes de sair da  clinica

--- lib/widgets_pckg.dart
+++ lib/widgets_pckg.dart
@@ -3,5 +3,4 @@ library widgets_pckg;
 export 'components/menu_list_tile_widget.dart';
 export 'components/menus_title_lt_widget.dart';
 export 'components/nav_drawer_widget.dart';
-// export 'icons/app_icons.dart';
 export 'theme_default/theme_default.dart';
